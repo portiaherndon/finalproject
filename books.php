abbe1271ac9b6bd975e1dcc8ba0e1b73242229f6
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-04-27 03:40:07 +0000

    building main webpage

---
+++ books.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<!-- Web Programming, Final Assignment (books)
+     This is the front page that displays the book categories when first opened -->
+	<script src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js" type="text/javascript"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/scriptaculous/1.9.0/scriptaculous.js" type="text/javascript"></script>
+	<script src="books.js" type="text/javascript"></script>
+	<head>
+		<title>Books!</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+	</body>
+<?php
+
+?>
+
+</html>
