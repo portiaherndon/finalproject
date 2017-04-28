a2474a39d39d2f4eed0d9c7427874c75cec6ddc3
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-04-28 01:31:35 +0000

    attempting to add function that will place book cateogries into an array

--- booklist.php
+++ booklist.php
@@ -1,38 +1,21 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<booklist>
-	<book category="Children">
-		<title>Goodnight Moon</title>
-		<author>Margaret Brown</author>
-		<year>1947</year>
-		<price>10.31</price>
-	</book>
-	<book category="Children">
-		<title>The Very Hungry Caterpillar</title>
-		<author>Eric Carle</author>
-		<year>1994</year>
-		<price>15.76</price>
-	</book>
-	<book category="Cooking">
-		<title>The Joy Of Cooking</title>
-		<author>Irma Rombauer</author>
-		<year>1931</year>
-		<price>15.75</price>
-	</book>
-	<book category="Cooking">
-		<title>The Food Lab</title>
-		<author>J. Kenji Lopez-Ant</author>
-		<year>2015</year>
-		<price>30.09</price>
-	</book>
-	<book category="Finance">
-		<title>I Will Teach You How to be Rich</title>
-		<author>Rammit Sethi</author>
-		<year>2009</yaer>
-		<price>7.89</price>
-	</book>
-	<book category="Financce">
-		<title>Think and Grow Rich</title>
-		<author>Napoleon Hill</author>
-		<year>1937</year>
-		<price>5.79</price>
-	</book>
+<?php 
+	$db= new PDO("mysql:dbname=books","root","Pherndon1234"); 
+	$categories=$db->query("SELECT * FROM book_category;");
+	foreach($categories as $category ){
+		$list_of_categories = $array();
+		for ($i =1; $i<=count($category);$i==){
+			$x = $i;
+			if($i === 1)
+				$list_of_categories = $category["category"];
+			while($x > 1)
+			{
+				if($category["category"] === $list_of_cateogories[$x-1])
+				{
+					$x--;		
+				}
+				else
+					$list_of_categories = $category;
+			}	
+			
+	} 
+?>
