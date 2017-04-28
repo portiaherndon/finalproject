b2e68e10bc17d77d5917f5b185b16987365d1714
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-04-28 02:00:17 +0000

    got rid of duplicates in book category array

--- booklist.php
+++ booklist.php
@@ -1,21 +1,25 @@
 <?php 
 	$db= new PDO("mysql:dbname=books","root","Pherndon1234"); 
 	$categories=$db->query("SELECT * FROM book_category;");
-	foreach($categories as $category ){
-		$list_of_categories = $array();
-		for ($i =1; $i<=count($category);$i==){
-			$x = $i;
-			if($i === 1)
-				$list_of_categories = $category["category"];
-			while($x > 1)
-			{
-				if($category["category"] === $list_of_cateogories[$x-1])
-				{
-					$x--;		
+	$test = False;
+	$book_categories = array();
+	foreach($categories as $category ){ 
+		//echo $category;
+		for($i=1;$i<count($category);$i++){
+			if($i>1){
+				$x=$i;
+				while($x >1){ 
+					if($category["category"] === $book_categories[$x-1]){
+					$test = True;
+					}
+					else
+						$test = False;
+					$x--;
 				}
-				else
-					$list_of_categories = $category;
-			}	
-			
+			}
+			if($test === False){
+				$book_categories = $category; 
+			}
+		}		
 	} 
 ?>
